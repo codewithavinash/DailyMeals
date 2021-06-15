a40bc922337d91c81a94497baa7b403ca086c8f5
codewithavinash <avinashthakur.edu@gmail.com>
2021-06-15 23:44:02 +0530

    error found

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:daily_meals/categories_screen.dart';
+import './categories_screen.dart';
 
 void main() {
   runApp(MyApp());
