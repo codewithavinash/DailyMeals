ec327a3e5e9d0ad9381bc2b76b6ef92139f37af8
codewithavinash <avinashthakur.edu@gmail.com>
2021-06-23 14:09:58 +0530

    dynamic AppBar Title changes with current Tab select

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -10,18 +10,13 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  final List<Widget> _pages = [
-    CategoriesScreen(),
-    FavouritesScreen(),
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {'page': FavouritesScreen(), 'title': 'Favourites'},
   ];
-
-  // final List<Map<String, Object>> _pages = [
-  //   {
-  //     'page': CategoriesScreen(),
-  //     'title': 'Categories',
-  //   },
-  //   {'page': FavouritesScreen(), 'title': 'Favourites'},
-  // ];
   int _selectedPageIndex = 0;
 
   // Fuction that gives automatic index of a Tab
@@ -39,12 +34,11 @@ class _TabsScreenState extends State<TabsScreen> {
     // child:
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meals'),
-        // title: _pages[_selectedPageIndex],
-        //title: _pages[_selectedPageIndex]['title'],
+        // title: Text('Meals'),
+        title: Text(_pages[_selectedPageIndex]['title']),
       ),
-      body: _pages[_selectedPageIndex],
-      //body: _pages[_selectedPageIndex]['page'],
+      // body: _pages[_selectedPageIndex],
+      body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         backgroundColor: Theme.of(context).primaryColor,
